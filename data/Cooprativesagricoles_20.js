4b16805d3303f3cd88f1daf2ea3bf0c5650a3d27
GabrielleBanqueAlimentaire <gabrielle.fontaine@banquealimentaire.org>
2022-11-28 16:51:08 +0100

    Cartographie BA974

---
+++ data/Cooprativesagricoles_20.js
@@ -0,0 +1,1 @@
+var json_Cooprativesagricoles_20 = {"type":"FeatureCollection","name":"Cooprativesagricoles_20","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
